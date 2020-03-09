e1f21f4945e6ba640423fb152240d24319808b96
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2020-03-09 14:10:35 -0700

    Update sritoni_cashfree.php

--- sritoni_cashfree.php
+++ sritoni_cashfree.php
@@ -1633,8 +1633,6 @@ function csv_to_associative_array($file, $delimiter = ',', $enclosure = '"')
     if (($handle = fopen($file, "r")) !== false)
     {
         $headers = fgetcsv($handle, 0, $delimiter, $enclosure);
-        error_log("headers array below:");
-        error_log(print_r($headers, true));
         return [];
         $lines = [];
         while (($data = fgetcsv($handle, 0, $delimiter, $enclosure)) !== false)
