4a640465442d466d459221382e1391efc49dd982
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2021-08-09 22:40:57 +0530

    dev

--- my_reconcile_form.js
+++ my_reconcile_form.js
@@ -1,14 +1,24 @@
 jQuery(document).ready(function($) 
 {
+  // initialize datatables with pre-drawn table done by HTML and PHP on page
   var table = $('#table-order-reconcile').DataTable();
 
   $('button').click( function() 
   {
+    // when the button on top of table is clicked
     // 
+    // the 1st column in the table contains the open orders
     var order_column_array =  table.columns().data()[0];
+
+    // serach for all inputs (which occur in column PaymentId). This data is serialized with idnames=paymentid&etc.
     var paymentidinputs = table.$('input').serialize();
+
+    // separate the string at the '&' into an array
     var payments_array = paymentidinputs.split('&');
+
+    // remove the prepend idname
     payments_array = payments_array.map(x => x.substring(x.indexOf('=') + 1));
+
     console.log(order_column_array);
     console.log(payments_array);
     
@@ -16,8 +26,8 @@ jQuery(document).ready(function($)
       my_reconcile_script_ajax_obj.ajax_url,              // url given in localize script in wordpress php
 			{                                                   // POST request
 				_ajax_nonce: my_reconcile_script_ajax_obj.nonce,  // nonce extracted and sent
-				action: "spzrbl_reconcile",         	            // hook added for action wp_ajax_spzrbl_city in php file
-				table_data: [order_column_array,payments_array]                           	// city from dropdown by user. This is accesed by server phphandler as $_POST['city']
+				action: "spzrbl_reconcile",         	            // hook added for action wp_ajax_spzrbl_reconcile in php
+				table_data: [order_column_array,payments_array]   // This is accessed in PHP handler as  $_POST['table_data']
 			},
             function(data_from_server) 					// data is JSON data sent back by server in response, wp_send_json($server_city_response)
 				{
