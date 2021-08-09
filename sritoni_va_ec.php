5eab63e20015fc5b190f9fceba788de798f8e6b1
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2021-08-09 16:15:52 +0530

    dev

--- sritoni_va_ec.php
+++ sritoni_va_ec.php
@@ -168,9 +168,11 @@ class sritoni_va_ec
     // get a list of open orders that are on-hold
     $ajax_call_data = $_POST['table_data'];
 
-    $paymentidinputs = $_POST['paymentidinputs'];
+    $orders_open = $ajax_call_data[0];
+    $payment_ids_input = $ajax_call_data[1];
 
-    error_log(print_r($ajax_call_data, true));
+    error_log(print_r($orders_open, true));
+    error_log(print_r($payment_ids_input, true));
 
   }
 
