bda9065210523e8caeda9cb208f828dc37e35483
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2021-08-09 14:17:00 +0530

    dev

--- my_reconcile_form.js
+++ my_reconcile_form.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function($)
 
   $('button').click( function() 
   {
-    var data =  table.column(':contains(Order)').data().serialize.toArray();
+    var data =  table.column(':contains(Order)').data();
     console.log(data);
     
     $.post(
