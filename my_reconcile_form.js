0578a696bc179d71f561bf111b3ee056cc426bb6
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2021-08-09 11:19:41 +0530

    dev

--- my_reconcile_form.js
+++ my_reconcile_form.js
@@ -4,24 +4,16 @@ jQuery(document).ready(function($)
 
   $('button').click( function() 
   {
-    var rows = [];
-    $("tbody tr").each(function () 
-    {
-      cur = {};
-      $(this).find("input").each(function(i, v) 
-      {
-        cur[i] = $(this).text().trim();
-      });
-      rows.push(cur);
-      cur = {};
-    });
+    var table_data =  { payment_ids:  table.$('input'),
+                        orders:       table.column('Order').data()
+  }
     
     $.post(
       my_reconcile_script_ajax_obj.ajax_url,              // url given in localize script in wordpress php
 			{                                                   // POST request
 				_ajax_nonce: my_reconcile_script_ajax_obj.nonce,  // nonce extracted and sent
 				action: "spzrbl_reconcile",         	            // hook added for action wp_ajax_spzrbl_city in php file
-				table_data: table.$('input')                                	// city from dropdown by user. This is accesed by server phphandler as $_POST['city']
+				table_data: table_data                               	// city from dropdown by user. This is accesed by server phphandler as $_POST['city']
 			},
             function(data_from_server) 					// data is JSON data sent back by server in response, wp_send_json($server_city_response)
 				{
